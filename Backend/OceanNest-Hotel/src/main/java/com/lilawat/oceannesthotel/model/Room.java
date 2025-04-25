cf2e9170072c379868f9bf8f184c124f199c6576
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-04-25 22:13:43 +0530

    Initial commit

--- Backend/OceanNest-Hotel/src/main/java/com/lilawat/oceannesthotel/model/Room.java
+++ Backend/OceanNest-Hotel/src/main/java/com/lilawat/oceannesthotel/model/Room.java
@@ -1,4 +1,49 @@
 package com.lilawat.oceannesthotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String RoomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+
+public void addBooking(BookedRoom booking) {
+        if (bookings == null) {
+            bookings = new ArrayList<>();
+        }
+
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+
+}
+
 }
